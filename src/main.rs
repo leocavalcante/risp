94e09560ff352bbbd95fe465288a02dd3af34484
leocavalcante <lc@leocavalcante.com>
2020-02-11 18:51:48 -0300

    Chooses random values from the given list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use rand::seq::IteratorRandom;
+
+type Whoops<T = ()> = Result<T, Box<dyn std::error::Error>>;
+
+fn rand(path: &String, value: &String) -> Whoops {
+    let value: usize = value.parse()?;
+    let mut rng = rand::thread_rng();
+
+    rdr.records()
+        .choose_multiple(&mut rng, value).iter()
+        .flat_map(|result| result)
+        .map(|record| record.as_slice())
+        .for_each(|slice| println!("{}", slice));
+
+    Ok(())
+}
+
+fn eval() -> Whoops {
+    let args = std::env::args();
+    let args: Vec<String> = args.collect();
+
+    let input_path = args.get(1).ok_or("input file is missing")?;
+    let command = args.get(2).ok_or("command not provided")?;
+
+    match command.as_str() {
+        "rand" => rand(input_path, args.get(3).ok_or("rand value not provided")?),
+        _ => {
+            return Err(format!("command {} not found", command).into());
+        }
+    }?;
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = eval() {
+        eprintln!("Something went wrong: {}", e);
+        std::process::exit(1);
+    }
 }
